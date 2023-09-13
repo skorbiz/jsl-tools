59fb38a502dd5108c8acb42187c63878a6d7eb1c
Johan S. Laursen <skorbiz@gmail.com>
2023-09-13 14:35:46 +0000

    Refactor to make the tool entry smaller

--- jla_tools/main.py
+++ jla_tools/main.py
@@ -4,6 +4,7 @@ import argparse
 from jla_tools.example.example import add_parsers as add_example_parser
 from jla_tools.terminal_animations.terminal_animations import add_parsers as add_terminal_animation_parser
 from jla_tools.devcontainer.dev import add_parsers as add_dev_parser
+from jla_tools.elastic.elastic import add_parsers as add_elastic_parser
 
 def parsers():
     parser = argparse.ArgumentParser(
@@ -16,6 +17,23 @@ def parsers():
     add_example_parser(subparsers)
     add_terminal_animation_parser(subparsers)
     add_dev_parser(subparsers)
+    add_elastic_parser(subparsers)
 
     argcomplete.autocomplete(parser)
     return parser
+
+
+def main():
+    parser = parsers()
+    args = parser.parse_args()
+
+    if 'func' in args:
+        return args.func(args)
+
+    print("""
+          Welcome to the JLA Tools CLI
+          """)
+
+    parser.print_usage()
+
+    return 255
